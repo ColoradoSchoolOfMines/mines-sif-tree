2e5ecf5b8a580dfe213c0d2c4abca0624ee60078
Umberto Gherardi <umberto.gherardi@gmail.com>
2023-12-19 13:39:18 -0700

    Add working suboption dropdowns

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-let options = document.getElementsByClassName("option");
+let primary_options = document.getElementsByClassName("option primary");
 
-for(let option of options) {
-    console.log(option);
-
+for(let option of primary_options) {
     option.addEventListener('click', () => {
-        console.log('clicked!');
+        target_dropdown = null;
+        switch (option.dataset.id) {
+            case "1":
+                target_dropdown = document.getElementById("dropdown1")
+                break;
+            case "2":
+                target_dropdown = document.getElementById("dropdown2");
+                break;
+            case "3":
+                target_dropdown = document.getElementById("dropdown3");
+                break;
+            default:
+                break;
+        }
+
+        if (target_dropdown.style.display == 'none' || 
+            target_dropdown.style.display == '') {
+            target_dropdown.style.display = 'block';
+        }
+        else {
+            target_dropdown.style.display = 'none';
+        }
     })
 }

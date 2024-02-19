b13dd5402da7295c2f2498d349b215ff0013030a
Jayden Pahukula <72293021+JaydenPahukula@users.noreply.github.com>
2024-02-18 19:34:25 -0700

    change to tab indentation (wordpress doesn't like spaces)

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 let primary_options = document.getElementsByClassName("option primary");
 
 for(let option of primary_options) {
-    option.addEventListener('click', () => {
-        target_dropdown = null;
-        switch (option.dataset.id) {
-            case "1":
-                target_dropdown = document.getElementById("dropdown1")
-                break;
-            case "2":
-                target_dropdown = document.getElementById("dropdown2");
-                break;
-            case "3":
-                target_dropdown = document.getElementById("dropdown3");
-                break;
-            default:
-                break;
-        }
+	option.addEventListener('click', () => {
+		target_dropdown = null;
+		switch (option.dataset.id) {
+			case "1":
+				target_dropdown = document.getElementById("dropdown1")
+				break;
+			case "2":
+				target_dropdown = document.getElementById("dropdown2");
+				break;
+			case "3":
+				target_dropdown = document.getElementById("dropdown3");
+				break;
+			default:
+				break;
+		}
 
-        if (target_dropdown.style.display == 'none' || 
-            target_dropdown.style.display == '') {
-            target_dropdown.style.display = 'block';
-        }
-        else {
-            target_dropdown.style.display = 'none';
-        }
-    })
+		if (target_dropdown.style.display == 'none' || 
+			target_dropdown.style.display == '') {
+			target_dropdown.style.display = 'block';
+		}
+		else {
+			target_dropdown.style.display = 'none';
+		}
+	})
 }

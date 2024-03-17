0d2d7a0e6e3433f073d008ab811f1c00531a0443
Umberto Gherardi <umberto.gherardi@gmail.com>
2024-03-16 18:32:16 -0600

    Add basic option selecting

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+const checkboxContainers = document.getElementsByClassName("checkbox-container");
+let selectedOptions = [];
 
+function addOption(option) {
+    selectedOptions.push(option);
+}
+
+function removeOption(optionToRemove) {
+    selectedOptions.splice(selectedOptions.indexOf(optionToRemove), 1);
+}
+
+for (let container of checkboxContainers) {
+    let checkbox = container.querySelector("input");
+    checkbox.addEventListener('click', () => {
+        if (checkbox.checked) {
+            addOption(checkbox.id);
+        } else {
+            removeOption(checkbox.id);
+        }
+    })
+}

d270cd1d3e76cb9a018a63341fa72ac257dba9e2
Umberto Gherardi <umberto.gherardi@gmail.com>
2024-03-16 18:49:19 -0600

    Add temp option output

--- script.js
+++ script.js
@@ -9,6 +9,27 @@ function removeOption(optionToRemove) {
     selectedOptions.splice(selectedOptions.indexOf(optionToRemove), 1);
 }
 
+function updateOptionsList() {
+    const oldOptions = document.getElementById("selected-options");
+    const newOptions = document.createElement("div");
+    const optionsList = document.createElement("ul");
+
+    for (i in selectedOptions) {
+        let listItem = document.createElement("li");
+        let optionText = document.createTextNode(selectedOptions[i]);
+        listItem.appendChild(optionText);
+        optionsList.append(listItem);
+    }
+
+    newOptions.appendChild(optionsList);
+    newOptions.id = "selected-options"
+    console.log(newOptions);
+    console.log(oldOptions);
+
+    document.body.replaceChild(newOptions, oldOptions);
+    console.log("Updated");
+}
+
 for (let container of checkboxContainers) {
     let checkbox = container.querySelector("input");
     checkbox.addEventListener('click', () => {
@@ -17,5 +38,6 @@ for (let container of checkboxContainers) {
         } else {
             removeOption(checkbox.id);
         }
+        updateOptionsList();
     })
 }

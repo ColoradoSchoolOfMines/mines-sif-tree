1bba24db4eab3e91b99ae7a6088af5d7942885b6
JaydenPahukula <jayden.pahukula@gmail.com>
2024-03-19 11:50:30 -0600

    basic functionality

--- script.js
+++ script.js
@@ -1,9 +1,146 @@
-import { data } from './data.js'
 
-data.Helios
+const data = {
+    "test": {
+        "link": "google.com",
+        "phase-solid": false,
+        "phase-liquid": false,
+        "solid_form-crystalline": false,
+        "solid_form-amorphous": false,
+        "solid_form-thin_film": false,
+        "solid_form-powder": false,
+        "solid_props-phase": false,
+        "solid_props-crystallorgraphic-texture": false,
+        "solid_props-strain": false,
+        "solid_props-bonding": false,
+        "spatial_res-na": false,
+        "spatial_res-mm": false,
+        "spatial_res-um": false,
+        "spatial_res-nm": false,
+        "dimensionality-1d": false,
+        "dimensionality-2d": false,
+        "dimensionality-3d": false,
+        "comp_sensitivity-na": false,
+        "comp_sensitivity-a_few_at_%": false,
+        "comp_sensitivity-ppm": false,
+        "temp-room": false,
+        "temp-heated": false,
+        "temp-cooled": false,
+        "mech_loading-na": false,
+        "mech_loading-tension": false,
+        "mech_loading-compression": false,
+        "mech_loading-bending": false,
+        "mech_loading-fatigue": false,
+        "atmosphere-ambient": false,
+        "atmosphere-vacuum": false,
+        "atmosphere-other": false,
+    },
+    "Helios": {
+        "link": "",
+        "phase-solid": true,
+        "phase-liquid": false,
+        "solid_form-crystalline": true,
+        "solid_form-amorphous": true,
+        "solid_form-thin_film": true,
+        "solid_form-powder": true,
+        "solid_props-phase": true,
+        "solid_props-crystallorgraphic-texture": true,
+        "solid_props-strain": false,
+        "solid_props-bonding": false,
+        "spatial_res-na": true,
+        "spatial_res-mm": true,
+        "spatial_res-um": true,
+        "spatial_res-nm": true,
+        "dimensionality-1d": true,
+        "dimensionality-2d": true,
+        "dimensionality-3d": true,
+        "comp_sensitivity-na": true,
+        "comp_sensitivity-a_few_at_%": true,
+        "comp_sensitivity-ppm": false,
+        "temp-room": true,
+        "temp-heated": false,
+        "temp-cooled": false,
+        "mech_loading-na": true,
+        "mech_loading-tension": false,
+        "mech_loading-compression": false,
+        "mech_loading-bending": false,
+        "mech_loading-fatigue": false,
+        "atmosphere-ambient": false,
+        "atmosphere-vacuum": true,
+        "atmosphere-other": false,
+    },
+    "Tescan": {
+        "link": "",
+    },
+    "JEOL": {
+        "link": "",
+    },
+    "Quanta": {
+        "link": "",
+    },
+    "Phenom": {
+        "link": "",
+    },
+    "TOF-SIMS": {
+        "link": "",
+    },
+    "LEAP": {
+        "link": "",
+    },
+    "SCIEX 5500": {
+        "link": "",
+    },
+    "SCIEX X500R": {
+        "link": "",
+    },
+    "OrbiTrap Exploris": {
+        "link": "",
+    },
+    "FTIR": {
+        "link": "",
+    },
+    "UV-Vis": {
+        "link": "",
+    },
+    "Ellipsometer": {
+        "link": "",
+    },
+    "SPM": {
+        "link": "",
+    },
+    "AFM": {
+        "link": "",
+    },
+    "Profilometer": {
+        "link": "",
+    },
+    "Keyence": {
+        "link": "",
+    },
+    "WITex Confocal": {
+        "link": "",
+    },
+    "DR-6000 Hach": {
+        "link": "",
+    },
+    "Shimadzu TOC-L": {
+        "link": "",
+    },
+    "Thermo Dionex ICS-900": {
+        "link": "",
+    },
+    "Panalytical Empyrean": {
+        "link": "",
+    },
+    "Panalytical X'Pert Pro": {
+        "link": "",
+    },
+    "Zeiss Versa XCT": {
+        "link": "",
+    },
+}
 
 const checkboxContainers = document.getElementsByClassName("checkbox-container");
-const instrumentsContainer = document.getElementById
+const instrumentsContainer = document.getElementById("instruments-panel");
 let selectedOptions = [];
 
 function addOption(option) {
@@ -25,7 +162,7 @@ function updateOptionsList() {
         let optionText = document.createTextNode(selectedOptions[i]);
         listItem.appendChild(optionText);
         optionsList.append(listItem);
-        options.append(optionText);
+        options.push(selectedOptions[i]);
     }
 
     newOptions.appendChild(optionsList);
@@ -35,10 +172,41 @@ function updateOptionsList() {
 
     document.body.replaceChild(newOptions, oldOptions);
     console.log("Updated");
+
+    updateInstruments(options)
 }
 
 function updateInstruments(options) {
-    
+    for (let name in data) {
+        let match = true;
+        for (let i = 0; i < options.length; i++) {
+            if (!data[name][options[i]]) {
+                match = false;
+                break;
+            }
+        }
+        let instrumentDiv = document.getElementById(name);
+        if (match) {
+            instrumentDiv.classList.remove("greyed-out");
+        } else {
+            instrumentDiv.classList.add("greyed-out");
+        }
+    }
+}
+
+// create all instruments in instrument panel
+for (let name in data) {
+    // create new div
+    let div = document.createElement('div');
+    div.id = name;
+    div.className = "instrument";
+    // create div contents
+    let a = document.createElement('a');
+    a.appendChild(document.createTextNode(name));
+    a.href = data[name].link;
+    div.appendChild(a);
+    // add to instruments panel
+    instrumentsContainer.appendChild(div);
 }
 
 for (let container of checkboxContainers) {
@@ -52,3 +220,5 @@ for (let container of checkboxContainers) {
         updateOptionsList();
     })
 }
+
+
